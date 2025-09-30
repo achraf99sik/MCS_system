e98a6ce0a05e0663e4f70e49dfffe489dcdf3714
achraf <achrafsikal@gmail.com>
2025-09-30 11:39:35 +0100

    MSS-28 feat: Implémenter la classe Incident.

--- src/enums/TypeIncident.java
+++ src/enums/TypeIncident.java
@@ -1,4 +1,9 @@
 package enums;
 
 public enum TypeIncident {
+    PAYE_A_TEMPS,
+    EN_RETARD,
+    PAYE_EN_RETARD,
+    IMPAYE_NON_REGLE,
+    IMPAYE_REGLE
 }
--- src/model/Incident.java
+++ src/model/Incident.java
@@ -1,5 +1,12 @@
 package model;
 
-public class Incident {
+import enums.TypeIncident;
 
+import java.time.LocalDate;
+
+public class Incident {
+    private LocalDate dateIncident;
+    private Echeance echeance;
+    private int score;
+    private TypeIncident typeIncident;
 }
