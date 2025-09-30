7e399a90e5a8932ca30fc84c9f17c89e0603583e
achraf <achrafsikal@gmail.com>
2025-09-30 11:31:01 +0100

    MSS-7 feat: Implémenter la classe abstraite Personne.

---
+++ src/enums/SituationFamiliale.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum SituationFamiliale {
+    CELIBATAIRE,
+    MARIE,
+    DIVORCE,
+    VEUF
+}
--- src/model/Personne.java
+++ src/model/Personne.java
@@ -1,5 +1,21 @@
 package model;
 
-public class Personne {
+import enums.SituationFamiliale;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+abstract class Personne {
+    private UUID id;
+    private String nom;
+    private String prenom;
+    private LocalDate datedenaissance;
+    private String ville;
+    private int nombreEnfants;
+    private String investissement;
+    private String placement;
+    private SituationFamiliale situationFamiliale;
+    private LocalDateTime createdAt;
+    private int score;
 }
